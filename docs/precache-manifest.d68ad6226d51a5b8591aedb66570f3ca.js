43889e2b3945cdb42a59f90abadb6ef54a5af057
Vishak Nambiar <intrinsic88@outlook.com>
2020-08-10 22:48:47 +0530

    added docs folder
    Docs folder added

---
+++ docs/precache-manifest.d68ad6226d51a5b8591aedb66570f3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74eb89efd2d4fac730eb7bc747a2b205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a01153c2cd854b01256",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39c06a59c74f3e7d5398",
+    "url": "/static/js/2.b27457b7.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/static/js/2.b27457b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a01153c2cd854b01256",
+    "url": "/static/js/main.f97f8d57.chunk.js"
+  },
+  {
+    "revision": "d13c95398c99db530c10",
+    "url": "/static/js/runtime-main.7976c2c1.js"
+  }
+]);
